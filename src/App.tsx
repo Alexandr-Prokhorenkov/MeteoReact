22a572a5bf59e1bbdffbf38793863737f5974888
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2025-03-09 22:26:41 +0300

    вуздщн

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Route, Routes  } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Header } from "./shared/Header/Header";
 
 function App() {
   return (
-    <>
-    <div className="container ">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
+    <Router>
+      <div className="container ">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-      </>
+    </Router>
   );
 }
 
