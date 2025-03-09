747204751da3ecf607b3b5c190e9e894e6357fc5
Aleksandr Prokhorenkov <89263379173@yandex.ru>
2025-03-09 22:36:00 +0300

    asasas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Header } from "./shared/Header/Header";
 
 function App() {
   return (
-    <Router>
-      <div className="container ">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="container ">
+      <Header />
+      <Routes>
+        <Route path="/MeteoReact" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
